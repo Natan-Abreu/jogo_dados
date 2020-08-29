3df79a13c1a087e83a505c8e7d13a2c201a22c11
Natan Abreu <guilhermenatanabreu@gmail.com>
2020-08-29 05:12:58 -0300

    Organização dos arquivos

---
+++ MainForm.Designer.cs
@@ -0,0 +1,154 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: aluno.etec
+ * Date: 30/09/2019
+ * Time: 20:43
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace jogo_dados
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+			this.button1 = new System.Windows.Forms.Button();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.button2 = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.BackColor = System.Drawing.Color.SaddleBrown;
+			this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button1.Location = new System.Drawing.Point(52, 72);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(243, 239);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "Descubra!";
+			this.button1.UseVisualStyleBackColor = false;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+			this.pictureBox1.Location = new System.Drawing.Point(301, 72);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(235, 239);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox1.TabIndex = 1;
+			this.pictureBox1.TabStop = false;
+			this.pictureBox1.Click += new System.EventHandler(this.PictureBox1Click);
+			// 
+			// label1
+			// 
+			this.label1.BackColor = System.Drawing.Color.Transparent;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(52, 0);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(484, 31);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "Qual número vai sair? Você tem 3 chances!";
+			// 
+			// label2
+			// 
+			this.label2.BackColor = System.Drawing.Color.Transparent;
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.Location = new System.Drawing.Point(301, 329);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(235, 34);
+			this.label2.TabIndex = 3;
+			this.label2.Text = "-";
+			this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// label3
+			// 
+			this.label3.BackColor = System.Drawing.Color.Transparent;
+			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label3.Location = new System.Drawing.Point(35, 329);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(260, 34);
+			this.label3.TabIndex = 5;
+			this.label3.Text = "Pontuação:";
+			this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(151, 34);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(301, 20);
+			this.textBox1.TabIndex = 6;
+			// 
+			// button2
+			// 
+			this.button2.BackColor = System.Drawing.Color.Transparent;
+			this.button2.Font = new System.Drawing.Font("Showcard Gothic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button2.Location = new System.Drawing.Point(112, 374);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(370, 32);
+			this.button2.TabIndex = 7;
+			this.button2.Text = "CLIQUE PARA REINICIAR A PARTIDA AGORA";
+			this.button2.UseVisualStyleBackColor = false;
+			this.button2.Click += new System.EventHandler(this.Button2Click);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+			this.ClientSize = new System.Drawing.Size(590, 414);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.button1);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "MainForm";
+			this.Text = "JODO DADOS - VERSÃO 0.0";
+			this.Load += new System.EventHandler(this.MainFormLoad);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Button button2;
+	}
+}
---
+++ MainForm.cs
@@ -0,0 +1,115 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: aluno.etec
+ * Date: 30/09/2019
+ * Time: 20:43
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace jogo_dados
+{
+	public partial class MainForm : Form
+	{
+		
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();			
+		}
+		int contAcertos=0, contClick=0;
+		Random rnd = new Random ();
+		
+		//Codificação BOTÃO DESCUBRA
+		void Button1Click(object sender, EventArgs e)
+		{
+			int n = rnd.Next(1, 7);
+			button1.Text = n.ToString();
+			
+			//Estrutura de decisão informando ao usuário se ele acertou o valor que sairá no botão
+			if (n == int.Parse(textBox1.Text)) {
+				MessageBox.Show("Acertou! Duvido que você consiga de novo...");
+				
+				contAcertos++;
+				label2.Text = contAcertos.ToString();
+				
+				textBox1.Focus();
+				textBox1.Text = " ";
+				
+			} else {
+				MessageBox.Show("Errou! Tente novamente!");
+				
+				textBox1.Focus();
+				textBox1.Text = " ";
+			}
+			
+			//Estrutura para quando o usuário usar todas as suas chances
+			contClick++;
+			if (contClick==3) {
+				MessageBox.Show("FIM DE JOGO");
+			}
+		}
+		
+		//Codificação PICTURE BOX INTERROGAÇÃO
+		void PictureBox1Click(object sender, EventArgs e)
+		{
+			int n = rnd.Next(1, 7);
+			pictureBox1.Load("dado"+n+".png");
+			
+			//Estrutura de decisão informando ao usuário se ele acertou o valor que sairá na PICTURE BOX
+			if ((int.Parse(textBox1.Text)<1) || (int.Parse(textBox1.Text)>6))
+			{
+				MessageBox.Show("Você não sabe quantos lados tem um dado?");
+			} 
+			else if ( n == int.Parse(textBox1.Text) )    {
+				MessageBox.Show("Acertou! Duvido que você consiga de novo...");
+				
+				textBox1.Clear();
+				textBox1.Focus();
+				
+				contAcertos++;
+				label2.Text = contAcertos.ToString();
+				
+				}
+			
+			else {
+				MessageBox.Show("Errou! Tente novamente!");
+				
+				textBox1.Clear();
+				textBox1.Focus();
+			}
+			
+			//Estrutura para quando o usuário usar todas as suas chances
+			contClick++;
+			if (contClick==3) {
+				pictureBox1.Enabled = false;
+				MessageBox.Show("FIM DE JOGO!");
+				/*MessageBox.Show("Deseja reiniciar a partida?");
+				return;*/
+			}
+		}
+		void MainFormLoad(object sender, EventArgs e)
+		{
+			
+		}
+		void TextBox1TextChanged(object sender, EventArgs e)
+		{
+	
+		}
+		
+		//PARA REINICIAR A PARTIDA
+		void Button2Click(object sender, EventArgs e)
+		{
+			textBox1.Focus();
+			textBox1.Text = " ";
+			
+			label2.Text = " - ";
+		}
+	}
+}
